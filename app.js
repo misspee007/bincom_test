712d9d1a8fdb5a58b3161d64594afaee551bfc7c
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-11-16 09:57:39 +0100

    debugging railway

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const appRouter = require('./src/routes/app.routes');
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
