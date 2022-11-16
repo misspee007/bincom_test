1d8ce88d93d9f0a3cb9de081fd3dc767e257dde1
Precious Abubakar <preciousdanabubakar@gmail.com>
2022-11-16 11:34:28 +0100

    railway db config

--- src/database/db.js
+++ src/database/db.js
@@ -2,19 +2,12 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 // Create connection
-// mysql://root:3DNDPB3NpKizRCGgQ7wl@containers-us-west-115.railway.app:7919/railway
-// mysql -hcontainers-us-west-115.railway.app -uroot -p3DNDPB3NpKizRCGgQ7wl --port 7919 --protocol=TCP railway
-// connect
 const db = mysql.createConnection({
-  connectionUri: process.env.MYSQL_CONECTION_URI,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
 });
-
-// const db = mysql.createConnection({
-// 	host: process.env.DB_HOST,
-// 	user: process.env.DB_USER,
-// 	password: process.env.DB_PASSWORD,
-// 	database: process.env.DB_NAME,
-// });
 
 // Connect to MySQL
 db.connect((err) => {
